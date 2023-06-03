ad0adbb0e4740a8379b58de22866d621d9c8ea60
peter-mwau <kyalepeter2000@gmail.com>
2023-06-03 15:45:20 +0300

    Updated

--- EBook/EBook/settings.py
+++ EBook/EBook/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$__cdc^ngz&vh2sdk=+^u%(3y$p_^rf8nzemvd=9da77d3r*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
