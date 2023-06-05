ab8790df1e0c1776424f15e1cdb2af3ca366aa41
peter-mwau <kyalepeter2000@gmail.com>
2023-06-05 19:24:21 +0300

    Updated

--- EBook/ebook/views.py
+++ EBook/ebook/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 import requests
+from .models import BookContent
 
 
 def home(request):
-    return render(request, 'home2.html')
+    # content = BookContent.objects.all()
+    # get title from BookContent
+    title = BookContent.objects.values_list('title', flat=True)
+    content = BookContent.objects.values_list('content', flat=True)
+
+    title = title[0]
+    content = content[0]
+
+    # save to dbase
+    # r = BookContent(title=title, content=content)
+    # r.save()
+
+    context ={
+        'titles': title,
+        'content': content,
+    }
+    return render(request, 'home2.html', context)
 
