e5c0c9dde1a9d25f238e34f74ab229139d82700a
peter-mwau <kyalepeter2000@gmail.com>
2023-06-06 17:29:39 +0300

    Updated

--- EBook/ebook/views.py
+++ EBook/ebook/views.py
@@ -4,21 +4,23 @@ from .models import BookContent
 
 
 def home(request):
-    # content = BookContent.objects.all()
+    content = BookContent.objects.all()
     # get title from BookContent
+    id = BookContent.objects.values_list('id', flat=True)
     title = BookContent.objects.values_list('title', flat=True)
     content = BookContent.objects.values_list('content', flat=True)
 
-    title = title[0]
-    content = content[0]
+    # print(title[0])
+    # print(content[0])
 
     # save to dbase
     # r = BookContent(title=title, content=content)
     # r.save()
 
     context ={
+        'id': id,
         'titles': title,
-        'content': content,
+        'contents': content,
     }
     return render(request, 'home2.html', context)
 
